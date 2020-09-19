7e14db5c1aea47a7071c101525dedf1ba8eb0f4a
reluss <paul_purkopp@gmx.de>
2020-09-20 01:31:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var btnGo = document.getElementById("go");
+
+function go() {
+  var btnAmount = document.getElementById("buttonsAmount").value;
+  console.log(btnAmount + " test");
+  var divSec = document.getElementById("btnTemp");
+  divSec.innerHTML = "";
+
+  for (i = 1; i <= btnAmount; i++) {
+    var createButtons = document.createElement("button");
+    createButtons.innerHTML = i;
+    divSec.appendChild(createButtons);
+    if (Number.isInteger(i / 10)) {
+      var createBreak = document.createElement("br");
+      divSec.appendChild(createBreak);
+    }
+    createButtons.setAttribute("id", i);
+    createButtons.setAttribute("class", "btnON");
+    createButtons.setAttribute("onClick", "markBtn(this.id)");
+  }
+
+  document.getElementById("resultCalc").innerHTML = btnAmount;
+}
+
+function markBtn(clicked_id) {
+  //alert(clicked_id);
+  var btnColor = document.getElementById(clicked_id);
+  if (btnColor.classList.contains("btnON")) {
+    btnColor.className = "btnOFF";
+  } else {
+    btnColor.className = "btnON";
+  }
+  var greenBtn = document.querySelectorAll(".btnON").length;
+  var redBtn = document.querySelectorAll(".btnOFF").length;
+  var result = greenBtn + redBtn - redBtn;
+  var resultP = document.getElementById("resultCalc");
+  resultP.innerHTML = greenBtn + redBtn + " - " + redBtn + " = " + result;
+  console.log(result);
+}
